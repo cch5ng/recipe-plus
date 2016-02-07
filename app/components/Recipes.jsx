1a3bbd497c4cb39bca857e887d4960ebdd492650
cch5ng <cch5ng@gmail.com>
2016-02-07 10:06:57 -0800

    chore: cleanup

--- app/components/Recipes.jsx
+++ app/components/Recipes.jsx
@@ -7,23 +7,13 @@ import {Modal} from 'react-bootstrap';
 import {Button} from 'react-bootstrap';
 import {Input} from 'react-bootstrap';
 
-//({recipes}) =>
-//???why does it matter whether you extend React.Component vs export {recipes, onEdit, onDelete}
-//is it better to store nameValid as state in App? and bubble up any changes back to App?
-
 export default class Recipes extends React.Component {
 	constructor(props) {
 		super(props);
-
-		this.state = {
-			//names : '',
-			nameValid: 'success'
-		}
 	}
 
 	render() {
 		var onDelete = this.props.onDelete;
-		var onEdit = this.props.onEdit;
 		var recipes = this.props.recipes;
 
 		return (
@@ -38,105 +28,4 @@ export default class Recipes extends React.Component {
 		);
 	}
 
-	getNames = () => {
-		let namesStr = '';
-		let namesCount;
-		namesCount = localStorage.length;
-		//console.log('namesCount: ' + namesCount);
-		for (let i = 0; i < namesCount; i++) {
-			//(namesCount - 1) prevents extra empty list item at the bottom
-			if (i < namesCount - 1) {
-				namesStr += localStorage.key(i) + ',';
-			} else {
-				namesStr += localStorage.key(i);
-			}
-		}
-		console.log('namesStr: ' + namesStr);
-		return namesStr;
-	};
-
-	getName = () => {
-		let curName = document.getElementById('recipeName');
-		return curName;
-	};
-
-	componentDidMount = () => {
-		let namesStr = this.getNames();
-		this.setState({names: namesStr});
-		this.render();
-	};
-
-	getNewName = () => {
-		let name = document.getElementById('recipeName').value;
-		console.log('new name: ' + name);
-		return name;
-	};
-
-	saveRecipe = (event) => {
-		if (this.state.nameValid === 'success') {
-			event.preventDefault();
-
-	//parsing the ingredients, cleaning up the format so it will display cleanly later on
-			// console.log('clicked save');
-			let existingNames = this.getNames();
-			let name = document.getElementById('recipeName').value;
-			console.log('name: ' + name);
-			var ingredientsStr = document.getElementById('recipeIngredients').value;
-			// console.log('ingredientsStr: ' + ingredientsStr);
-			var ingredientsAr = ingredientsStr.split(',');
-		//stores final array of ingredients strings, trimmed
-			var ingredientsTrim = [];
-			// console.log('ingredientsAr: ' + ingredientsAr);
-			// console.log('length ingredientsAr: ' + ingredientsAr.length);
-			ingredientsAr.forEach(function(item) {
-				// console.log('item: ' + item);
-				var itemCopy = item.slice(0).trim();
-				ingredientsTrim.push(itemCopy);
-			});
-			//making the ingredients list in localStorage comma delimited but no space
-			var ingredientsStrClean = ingredientsTrim.join(',');
-			//console.log('ingredientsStrClean: ' + ingredientsStrClean);
-
-	//updating localStorage
-			localStorage.setItem(name, ingredientsStrClean);
-			//console.log('names: ' + this.getNames());
-
-			let namesStr;
-			namesStr = existingNames + ',' + name;
-			//console.log('save namesStr: ' + namesStr);
-			this.setState({names: namesStr});
-
-			let form = document.getElementById('recipeForm');
-			form.reset();
-		}
-	};
-
-	//perform validation to ensure that name field is unique (key in localStorage)
-	validationState = (event) => {
-		let matchCount;
-		let curName = event.target.value;
-		if (localStorage.getItem(curName)) {
-			this.setState({nameValid: 'error'});
-		} else {
-			this.setState({nameValid: 'success'});
-		}
-	};
-
-	handleDelete = (name) => {
-		let namesStr;
-		namesStr = this.state.names;
-		let namesAr = namesStr.split(',');
-		let newNamesAr = [];
-		let newNamesStr;
-
-		namesAr.forEach(function(mname) {
-			if (mname !== name) {
-				newNamesAr.push(mname);
-			}
-		});
-		//console.log('newNamesAr: ' + newNamesAr);
-		this.setState({names: newNamesStr});
-	};
-
-//});
 }
