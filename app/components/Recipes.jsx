a3e03efb70295c6d0df69500cb19afd8647254c9
cch5ng <cch5ng@gmail.com>
2016-02-28 23:42:06 -0800

    fix: revert to stateless function class

--- app/components/Recipes.jsx
+++ app/components/Recipes.jsx
@@ -8,27 +8,9 @@ import {Button} from 'react-bootstrap';
 import {Input} from 'react-bootstrap';
 
 //TODO update this to regular component since it should store state now
-{/*export default ({recipes, onDelete}) => { */}
-export default class Recipes extends React.Component {
-	constructor(props) {
-		super(props);
-		let namesAr = [];
-		var recipes = [];
-		namesAr = this.getNames().split(',');
-		namesAr.forEach(name => {
-			let mrecipe = {};
-			mrecipe.name = name;
-			mrecipe.id = uuid.v4();
-			recipes.push(mrecipe);
-		});
-		this.state = {
-			recipes: recipes,
-			//show: false,
-			//nameValid: 'success'
-		}
-	}
+export default ({recipes, onDelete}) => {
+{/*export default class Recipes extends React.Component */}
 
-	render() {
 		return (
 			<div className="recipeList">
 				<ul>
@@ -38,28 +20,5 @@ export default class Recipes extends React.Component {
 				</ul>
 			</div>
 		)
-	}
 
-	/**
-	 * Gets recipe names from localStorage
-	 * @return {String} Comma-delimited list of names
-	 */
-//TODO, need to restructure this for the new data model in localStorage
-	getNames = () => {
-		let namesStr = '';
-		let namesCount;
-		namesCount = localStorage.length;
-		//console.log('namesCount: ' + namesCount);
-		for (let i = 0; i < namesCount; i++) {
-			//(namesCount - 1) prevents extra empty list item at the bottom
-			if (i < namesCount - 1) {
-				namesStr += localStorage.key(i) + ',';
-			} else {
-				namesStr += localStorage.key(i);
-			}
-		}
-		//console.log('namesStr: ' + namesStr);
-		//console.log('storage count: ' + namesCount);
-		return namesStr;
-	};
 }
